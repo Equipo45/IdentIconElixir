b50f5a81ed91716b9816c570ff456343b37bed26
Equipo45 <ismiruiz@gmail.com>
2023-03-02 22:10:48 +0100

    add image logic regarding hexadecimal

--- lib/identiconelixir.ex
+++ lib/identiconelixir.ex
@@ -12,7 +12,21 @@ defmodule Identiconelixir do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |>hash_input
+    |> pick_color
+  end
+
+  #At the same time we recieved the image , we deconstruct it to get the colors at the same time
+  def pick_color(%Identiconelixir.Image{hex: [red, green, blue | _ ]} = image) do
+    %Identiconelixir.Image{image | color: {red, green, blue}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5,input)
+    |>:binary.bin_to_list
+
+    %Identiconelixir.Image{hex: hex}
   end
 end
---
+++ lib/image.ex
@@ -0,0 +1,3 @@
+defmodule Identiconelixir.Image do
+  defstruct hex: nil, color: nil #If you want to add a new argument to a struct it must be here
+end
