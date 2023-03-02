958a74ad6705066a0dde6c4d07a97524ff6b2908
Equipo45 <ismiruiz@gmail.com>
2023-03-02 16:46:33 +0100

    add initial logic

---
+++ lib/identiconelixir.ex
@@ -0,0 +1,18 @@
+defmodule Identiconelixir do
+  @moduledoc """
+  Documentation for `Identiconelixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identiconelixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/identiconelixir_test.exs
@@ -0,0 +1,8 @@
+defmodule IdenticonelixirTest do
+  use ExUnit.Case
+  doctest Identiconelixir
+
+  test "greets the world" do
+    assert Identiconelixir.hello() == :world
+  end
+end
